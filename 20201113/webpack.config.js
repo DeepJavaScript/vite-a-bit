6b3e1eaf1446678eda3df03938531c4058c03e67
yachen <io260188@gmail.com>
2020-11-08 14:23:44 +0800

    feat: rename index.js to main.js

--- 20201113/webpack.config.js
+++ 20201113/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
