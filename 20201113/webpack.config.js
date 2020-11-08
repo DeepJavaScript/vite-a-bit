d17729f4c2d0671bf42b5fea950c99942f70e9a8
yachen <io260188@gmail.com>
2020-11-08 13:16:39 +0800

    feat: disable transform files into base64

--- 20201113/webpack.config.js
+++ 20201113/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         loader: "url-loader",
         options: {
-          limit: 8192,
+          limit: false,
           outputPath: 'images'
         }
       },
