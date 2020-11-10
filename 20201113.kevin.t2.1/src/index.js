7077ffcad8ee3e94dd2046e286061fce1b9b02d0
OTree <kevin10410@gmail.com>
2020-11-10 22:49:07 +0800

    refactor(index.js): refactor index.html into index.js

--- 20201113.kevin.t2.1/src/index.js
+++ 20201113.kevin.t2.1/src/index.js
@@ -1,1 +1,23 @@
-require('./index.css')
+import './index.css';
+
+function app() {
+  const appElement = document.createElement('div');
+  appElement.setAttribute("id", "app");
+
+  const bannerElement = document.createElement('div');
+  bannerElement.className = 'banner';
+  bannerElement.innerHTML = `
+    <img
+      src="./assets/logo.png"
+      width="100"
+      alt="vue"
+      class="logo"
+    />
+    <h1>Welcome to Vue.js</h1>
+  `;
+
+  appElement.appendChild(bannerElement);
+  return appElement;
+};
+
+document.body.appendChild(app());
