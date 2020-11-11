7fbeef1cac2a945428b85a6214f5b3c7af741552
YUN-RU-TSENG <coco002197@gmail.com>
2020-11-12 02:21:23 +0800

    [style] format code

--- T1/src/main.js
+++ T1/src/main.js
@@ -1,24 +1,24 @@
 // 樣式重置
 import './assets/style/reset.css'
-// fontawesome
+
+// fontawesome 資源
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faGooglePlusSquare, faFacebookSquare, faTwitterSquare, faPinterestSquare } from '@fortawesome/free-brands-svg-icons'
 
-// 加載組件
+// 組件
 import { createArticleComponent } from './component/Article/article'
 import { createNavbarComponent } from './component/Navbar/navbar'
 import { createFooterComponent } from './component/footer/footer'
 
-// 行不通
-// library.add(faCamera)
-// const camera = icon({ prefix: 'fas', iconName: 'camera' })
-
+// 頁面渲染組件
 document.body.appendChild(createNavbarComponent())
 document.body.appendChild(createArticleComponent())
 document.body.appendChild(createFooterComponent())
 
+// fontawesome
 library.add(faGooglePlusSquare)
 library.add(faFacebookSquare)
 library.add(faTwitterSquare)
 library.add(faPinterestSquare)
+// fontawesome 替換 fontawesome icon 函式
 dom.watch()
