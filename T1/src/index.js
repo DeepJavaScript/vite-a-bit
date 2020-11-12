b9f4c41c7d4f15c94a73ba3180cfda46ec63ea67
Larry <shaka422@hotmail.com>
2020-11-12 16:49:08 +0800

    feat webpack bundle done

--- T1/src/index.js
+++ T1/src/index.js
@@ -1,3 +1,2 @@
-import _ from 'lodash';
 import './css/index.css';
 import './css/css/all.css';
---
+++ T1/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[hash:10][ext][query]'
+                }
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'svg/[hash:10][ext][query]'
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    mode: 'development'
+}
