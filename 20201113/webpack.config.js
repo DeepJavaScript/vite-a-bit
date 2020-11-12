b86289c61b072f376a496ca4dfd6a855674acab3
yachen <io260188@gmail.com>
2020-11-12 14:25:37 +0800

    build: clean-webpack-plugin

--- 20201113/webpack.config.js
+++ 20201113/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/main.js",
@@ -9,6 +10,7 @@ module.exports = {
     publicPath: './'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
