ed83d183f42f9d55510008f8d3cfbf09db6244d4
Penghua <kids5346@gmail.com>
2020-11-12 23:15:55 +0800

    t1

---
+++ T1/src/index.js
@@ -0,0 +1,6 @@
+import "./index.css";
+import ideaPic from "./assets/img/ideas-pic.png";
+
+document
+  .querySelector('.ideas-pic img')
+  .setAttribute("src", ideaPic);
---
+++ T1/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        },
+      },
+    ],
+  },
+};
