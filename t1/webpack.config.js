c13790f75625f332f87796f8c5237fdccc27c9f1
JinWen <onlystp417@gmail.com>
2020-11-12 16:55:43 +0800

    [Feat] import css in index.js and automaticly build index.html and img

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: 'index.html'
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash:5][ext][query]',
+    publicPath: './'
   },
   module: {
     rules: [
@@ -15,6 +26,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
       },
     ],
   },
