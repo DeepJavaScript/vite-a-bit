b9ce9fefe8edb1fcdfb869acba67d7c8f7032685
g100my <g100my.lan229@gmail.com>
2020-11-13 00:09:05 +0800

    install clean-webpack-plugin

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -6,5 +7,8 @@ module.exports = {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	plugins: [
+		new CleanWebpackPlugin(),
+	],
 	mode: 'development',
 };
