47b2909848a703c040697655cbf726153f070f1f
g100my <g100my.lan229@gmail.com>
2020-11-13 02:49:05 +0800

    feat: replace fontawesome css file

--- t1/src/index.js
+++ t1/src/index.js
@@ -1,2 +1,3 @@
 import "./css/main.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
