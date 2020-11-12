0a68e977cf09b3ebcdcfa24880d30877533ea50a
titangene <titangene.tw@gmail.com>
2020-11-12 19:58:28 +0800

    feat(asset-mgmt): import image in JS & CSS

--- asset-management/src/index.js
+++ asset-management/src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 import './style.css';
+import Icon from './webpack-icon.png';
 
 function component() {
   const element = document.createElement('div');
@@ -8,6 +9,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
