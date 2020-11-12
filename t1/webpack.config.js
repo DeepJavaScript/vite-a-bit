fb29b6ed4b016daefee01101f1543da09472c526
acid7820@gmail.com <acid7820@gmail.com>
2020-11-13 00:52:13 +0800

    docs: add loader setting in webpack.config.js

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -9,5 +9,17 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+		},
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg)$/,
+					use: ['file-loader']
+				},
+			],
+		},
 };
