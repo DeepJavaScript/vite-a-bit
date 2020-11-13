4c49571aa6c96359cab761177d2714dd78c370c7
cshif <cooshif@gmail.com>
2020-11-13 13:58:31 +0800

    bundled t1 by webpack

---
+++ t1-webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './',
+    assetModuleFilename: 'img/[hash][ext][query]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 't1-webpack-eva',
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
+};
