e6cba5197e50bbb87131b1bb34b7ec53c72043c7
titangene <titangene.tw@gmail.com>
2020-11-13 10:57:30 +0800

    build(t1): use html-webpack-plugin

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -7,7 +8,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
   ],
   module: {
     rules: [
