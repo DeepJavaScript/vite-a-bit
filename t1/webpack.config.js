52debb53cf505235b641871d13869c2ec5152d94
titangene <titangene.tw@gmail.com>
2020-11-13 10:53:07 +0800

    build(t1): use clean-webpack-plugin

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
   module: {
     rules: [
       {
