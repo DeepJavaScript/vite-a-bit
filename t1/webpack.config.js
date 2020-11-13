07a310ffb869ec887d46e22112a3ad1f0de6f317
g100my <g100my.lan229@gmail.com>
2020-11-13 16:31:28 +0800

    feat: limit hash name length

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -45,10 +45,16 @@ module.exports = {
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: 'asset/resource',
+				generator: {
+					filename: '[hash:5][ext]'
+				}
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
 				type: 'asset/resource',
+				generator: {
+					filename: 'font-type/[hash:5][ext]'
+				}
 			},
 		],
 	},
