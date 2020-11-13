c33d6469164307f207a4b8833897a2e8ab314d70
yachen <io260188@gmail.com>
2020-11-13 13:16:01 +0800

    docs: rename

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: "./src/main.js",
   output: {
     filename: "js/bundle.js",
