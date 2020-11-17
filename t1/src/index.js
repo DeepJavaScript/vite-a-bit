be6cdc3209b5c769b1002319292625c7839f77c7
JinWen <onlystp417@gmail.com>
2020-11-17 13:58:11 +0800

    [Fix] Pack fonts to vender/ and Pack styleSheet to css/

--- t1/src/index.js
+++ t1/src/index.js
@@ -1,2 +1,3 @@
 import './style.css';
-import '@fortawesome/fontawesome-free/js/all.js';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/main.css'
+    }),
     new HtmlWebpackPlugin({
       title: 'Output Management',
       template: 'index.html'
@@ -14,18 +18,32 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash:5][ext][query]',
+    // assetModuleFilename: 'images/[hash:5][ext][query]',
     publicPath: './'
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        // 把 css 打包出去 https://webpack.js.org/plugins/mini-css-extract-plugin/
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        generator: {
+          filename: 'css/[hash][ext][query]'
+        }
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash:8][ext][query]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'vender/[hash:8][ext][query]'
+        }
       },
       {
         test: /\.html$/i,
