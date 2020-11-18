2cf9d56df30c41efed7fa311428dd264f1dcaca3
g100my <g100my.lan229@gmail.com>
2020-11-18 18:02:49 +0800

    T2 finish

---
+++ t2/webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+// const webpack = require('webpack');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+
+  output: {
+    filename: '[name][hash:5]].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: ""
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'image/[name][hash:5][ext]'
+        }
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ]
+  },
+}
