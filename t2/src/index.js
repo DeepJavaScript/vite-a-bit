06bd1b8255c2d334c6950af55ba29c504da44f07
g100my <g100my.lan229@gmail.com>
2020-11-18 19:40:15 +0800

    import bootstrap js/css

--- t2/src/index.js
+++ t2/src/index.js
@@ -1,1 +1,8 @@
 import "./css/main.css"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 預設是 "main": "dist/js/bootstrap.js"
+// import "bootstrap"
+// 不想額外安裝 popper.js 所以用 bundle 版本
+import "bootstrap/dist/js/bootstrap.bundle.js"
