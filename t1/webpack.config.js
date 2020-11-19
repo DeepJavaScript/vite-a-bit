3d8431fd56abd997e8e354906094706eea64cdf1
acid7820@gmail.com <acid7820@gmail.com>
2020-11-19 22:54:24 +0800

    feat: rendering index.html and add html-loader、html-webpack-plugin、clean-webpack-plugin, remove file-loader use webpack5 asset modules asset/resource rendering images

--- t1/webpack.config.js
+++ t1/webpack.config.js
@@ -1,6 +1,9 @@
 //nodejs 要指定任何檔案路徑都要靠叫 path 的 api 來做，用 require 的方式來引入 path 的模組
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     // entry:進入點，專案執行時先從 index.js 開始
     entry: './src/index.js',
@@ -8,7 +11,8 @@ module.exports = {
     // output:輸出點，專案打包後輸出的檔名和檔案路徑
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+				path: path.resolve(__dirname, 'dist'),
+				publicPath: './'
 		},
 		module: {
 			rules: [
@@ -18,8 +22,21 @@ module.exports = {
 				},
 				{
 					test: /\.(jpe?g|png|gif|svg)$/,
-					use: ['file-loader']
+					type: 'asset/resource',
+					generator: {
+						filename: 'img/[name].[hash:8][ext]'
+					}
 				},
+				{
+					test: /\.html$/,
+					loader: 'html-loader'
+				}
 			],
 		},
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin({
+				template: './src/index.html'
+			}),
+		],
 };
