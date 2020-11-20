10a3cb6f58be1cf3fe5a882568ad2ba090adf512
titangene <titangene.tw@gmail.com>
2020-11-20 10:44:58 +0800

    chore(t3): init project

---
+++ t3/src/main.js
@@ -0,0 +1,5 @@
+import './main.css';
+
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
