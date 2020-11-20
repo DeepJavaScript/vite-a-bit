8528a127459b0236e164be7cd9a7228689c75669
chris <dwatow@gmail.com>
2020-11-20 18:50:48 +0800

    add MiniCssExtractPlugin for webpack

--- t3/webpack.config.js
+++ t3/webpack.config.js
@@ -10,33 +10,56 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    open: true,
-    port: 8080
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       title: 'Development',
       template: "./src/index.html",
+      // meta: {
+      //   viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      // },
     }),
+    // version must @next
+//     (node:94791) [DEP_WEBPACK_COMPILATION_ASSETS] DeprecationWarning: Compilation.assets will be frozen in future, all modifications are deprecated.
+// BREAKING CHANGE: No more changes should happen to Compilation.assets after sealing the Compilation.
+//         Do changes to assets earlier, e. g. in Compilation.hooks.processAssets.
+//         Make sure to select an appropriate stage from Compilation.PROCESS_ASSETS_STAGE_*.
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: './',
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: "img/[name].[hash:4][ext]"
+        }
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //   ],
+      // },
+      {
+        test: /\.css$/i,
+        use:  [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './'
+            }
+          },
+          'css-loader'
         ],
       },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        type: 'asset/resource'
-      }
     ],
   },
   optimization: {
