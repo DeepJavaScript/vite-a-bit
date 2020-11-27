15e4231afd8628630e6d1e19016a8e1ebd3707ad
YUN-RU-TSENG <coco002197@gmail.com>
2020-11-27 14:34:18 +0800

    [refactor] component function refactor

--- t5/src/views/home/component.js
+++ t5/src/views/home/component.js
@@ -1,15 +1,18 @@
+// 引入全局樣式
 import '../../assets/style/main.scss'
+
+// 引入組件樣式
 import header from '../../components/header/component'
 import main from '../../components/main/component'
 import footer from '../../components/footer/component'
 
+// 生成 Home 樣式
 const tagName = 'div'
 const template = `
-      <div class="container custom_container">
-        ${header}
-        ${main}
-        ${footer}
-      </div>`
+    ${header}
+    ${main}
+    ${footer}
+`
 
 export default {
   tagName,
