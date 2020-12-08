d414d33540777f4682014418ced2e2bd52a5c556
G100my <G100my.lan229@gmail.com>
2020-12-08 15:28:42 +0800

    can pack normally

---
+++ js30/src/index.js
@@ -0,0 +1,55 @@
+import "./style.css";
+
+  const holes = document.querySelectorAll('.hole');
+  const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const startButton = document.getElementById("start-button");
+console.log(startGame)
+  let lastHole;
+  let timeUp = false;
+  let score = 0;
+
+  function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  function randomHole(holes) {
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+
+    if (hole === lastHole) return randomHole(holes);
+
+    lastHole = hole;
+    return hole;
+  }
+
+  function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+      hole.classList.remove('up');
+      console.log(timeUp)
+      if (!timeUp) peep();
+    }, time)
+  }
+
+  function startGame() {
+    scoreBoard.textContent = 0;
+    score = 0;
+    timeUp = false;
+    peep();
+    setTimeout(() => timeUp = true, 2000) 
+  }
+
+
+  startButton.addEventListener("click", startGame)
+
+  function bonk(event) {
+    if (!event.isTrusted) return
+    this.parentNode.classList.remove('up');
+    score++;
+    scoreBoard.textContent = score;
+  }
+
+  moles.forEach(mole => mole.addEventListener('click', bonk));
---
+++ js30/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: ""
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 9000,
+		hot: true,
+	},
+	plugins: [
+		// new CleanWebpackPlugin({
+		// 	// cleanStaleWebpackAssets: false,
+		// }),
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader',
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'image/[name][ext]'
+				}
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+		]
+	},
+}
