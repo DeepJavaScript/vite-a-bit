8610c2e19af8a0687c6677d6088853878181aa66
G100my <G100my.lan229@gmail.com>
2020-12-08 17:12:13 +0800

    prettier init

---
+++ js30/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 12,
+	semi: false,
+	useTabs: true,
+	printWidth: 100,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	bracketSpacing: true,
+}
