d79a9f74eed36ab301e302f48c3e31df09e25236
JinWen <onlystp417@gmail.com>
2020-12-10 16:06:13 +0800

    [Build] ESLint configration

---
+++ Whac_A_Mole/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    // "node": true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    // "curly": "error",
+  }
+};
