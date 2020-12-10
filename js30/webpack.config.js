78bcf46436b7d7d3e52f253057bd969e8a03e3af
Larry <shaka422@hotmail.com>
2020-12-10 11:40:53 +0800

    webpack 設定完成

---
+++ js30/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[hash:10][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index-FINISHED.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].bundle.css",
+            chunkFilename: '[id].css',
+        })
+    ],
+    devServer: {
+        contentBase: './dist',
+        open: 'Google Chrome',
+        compress: true
+    },
+}
