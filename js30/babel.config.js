9842d5a42fda1e1c3069e6532b313819882d66a8
G100my <G100my.lan229@gmail.com>
2020-12-11 01:21:54 +0800

    add babel.config.js

---
+++ js30/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"presets": [
+	[
+		// README.md Q1
+		"@babel/env",
+		{
+			// default value is "{}", if wanna set it to default, you have to explicitly pass it: "default".
+			// will be change in babel v8
+			"targets": {
+			"edge": "17",
+			"firefox": "60",
+			"chrome": "67",
+			"safari": "11.1"
+			},
+			"useBuiltIns": "usage",
+			"corejs": "3.6.5",
+			// bugfixes defaults to false, but enabled by default in bable 8, how about babel-loader ^8 ??
+			"bugfixes": false,
+			"debug": true,
+		}
+	  ]
+	]
+  }
