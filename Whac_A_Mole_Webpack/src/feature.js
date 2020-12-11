adfa71c5e3e2f2b6d113060287eedef5bf550692
JinWen <onlystp417@gmail.com>
2020-12-11 15:44:14 +0800

    [Style] JS formatting

--- Whac_A_Mole_Webpack/src/feature.js
+++ Whac_A_Mole_Webpack/src/feature.js
@@ -2,6 +2,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 const starButton = document.querySelector('.starButton');
+
 let lastHole;
 let timeUp = false;
 let score = 0;
