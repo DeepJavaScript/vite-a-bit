2f635bd76558efec30d358ca1e27dfa41cfbd515
G100my <G100my.lan229@gmail.com>
2020-12-11 11:28:52 +0800

    change "targets" value to browserlist query

--- js30/babel.config.js
+++ js30/babel.config.js
@@ -6,13 +6,15 @@ module.exports = {
 		{
 			// default value is "{}", if wanna set it to default, you have to explicitly pass it: "default".
 			// will be change in babel v8
-			"targets": {
-			"edge": "17",
-			"firefox": "60",
-			"chrome": "67",
-			"safari": "11.1"
-			},
-			"useBuiltIns": "usage",
+			// "targets": {
+			// "edge": "17",
+			// "firefox": "60",
+			// "chrome": "67",
+			// "safari": "11.1"
+			// },
+			"targets": "> 0.25%, not dead",
+			// "useBuiltIns": "usage",
+			"useBuiltIns": "entry",
 			"corejs": "3.6.5",
 			// bugfixes defaults to false, but enabled by default in bable 8, how about babel-loader ^8 ??
 			"bugfixes": false,
