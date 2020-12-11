cb294c9ca64c9bddfd9be8f61e3521ba6f1365ca
titangene <titangene.tw@gmail.com>
2020-12-11 15:01:54 +0800

    build(js30): use eslint-webpack-plugin

--- js30/webpack.config.js
+++ js30/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -23,7 +24,8 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new ESLintPlugin()
   ],
   module: {
     rules: [
