c4dd595502166d61bd3ba4915fcff0d4043483b2
titangene <titangene.tw@gmail.com>
2020-12-11 08:24:02 +0800

    feat(js30): add code for testing Babel polyfill

--- js30/src/main.js
+++ js30/src/main.js
@@ -48,3 +48,41 @@ function bonk(e) {
 }
 
 moles.forEach(mole => mole.addEventListener('click', bonk));
+
+// ===================================
+// 測試 Babel polyfill
+
+// generator function
+function* generatorFunc(i) {
+  yield i + 1;
+  yield i + 2;
+  yield i + 3;
+}
+const generator = generatorFunc(10);
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next().value);
+
+// # ES2016
+const ES2016 = 'xxx'.includes('x');
+console.log(ES2016);
+
+// # ES2017
+const ES2017 = Object.entries({ a: 1 });
+console.log(ES2017);
+
+// # ES2018
+const ES2018 = Promise.resolve(1).finally();
+console.log(ES2018);
+
+// # ES2019
+const ES2019 = Object.fromEntries([["a", 1]]);
+console.log(ES2019);
+
+// # ES2020
+const ES2020 = 'abc'.matchAll(/\w/g);
+console.log([...ES2020]);
+
+// # ES2021
+const ES2021 = null ?? 'default';
+console.log(ES2021);
