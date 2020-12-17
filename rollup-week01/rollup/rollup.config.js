8b8be88c296c41911192c6d13fecc1d047acd819
YUN-RU-TSENG <coco002197@gmail.com>
2020-12-18 00:42:20 +0800

    [chore] remove commonjs plugin

--- rollup-week01/rollup/rollup.config.js
+++ rollup-week01/rollup/rollup.config.js
@@ -1,28 +1,25 @@
 import del from 'rollup-plugin-delete'
-import postcss from 'rollup-plugin-postcss'
-import { terser } from 'rollup-plugin-terser'
-import commonjs from '@rollup/plugin-commonjs'
-import globals from 'rollup-plugin-node-globals'
-import resolve from '@rollup/plugin-node-resolve'
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
-import htmlTemplate from 'rollup-plugin-generate-html-template'
-import visualizer from 'rollup-plugin-visualizer'
+import postcss from 'rollup-plugin-postcss';
+import { terser } from "rollup-plugin-terser";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+import globals from "rollup-plugin-node-globals";
+import resolve from "@rollup/plugin-node-resolve";
+import htmlTemplate from 'rollup-plugin-generate-html-template';
 
 export default {
-  input: './main.js',
+  input: "./main.js",
   output: {
     dir: 'dist',
-    format: 'iife',
+    format: "iife",
     sourcemap: true,
-    entryFileNames: '[name].[hash].js', // ! why
-    chunkFileNames: '[name].[hash][extname]'
+    entryFileNames: '[name].[hash].js',
+    chunkFileNames: '[name].[hash][extname]',
   },
   plugins: [
+    terser(),
     sizeSnapshot(),
     del({ targets: 'dist/*' }), // delete useless file before bundle
-    terser(), // minify javascript file
     resolve({ browser: true }), // tells Rollup how to find date-fns in node_modules
-    commonjs(), // converts date-fns to ES modules
     globals(),
     postcss({
       extensions: ['.scss', '.css', '.sass'],
@@ -32,10 +29,7 @@ export default {
     }),
     htmlTemplate({
       template: './index.html',
-      target: 'dist/index.html'
+      target: 'dist/index.html',
     }),
-    visualizer({
-      gzipSize: true
-    })
-  ]
-}
+  ],
+};
