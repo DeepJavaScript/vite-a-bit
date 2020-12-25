2ab05fc29ab5e91f8a6d825f4a831f4d99aa8a0a
titangene <titangene.tw@gmail.com>
2020-12-25 08:26:07 +0800

    feat(vite-HW): use `@` alias

--- vite-hello-world/src/views/Home.vue
+++ vite-hello-world/src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="/@/assets/logo.png" />
+    <img alt="Vue logo" src="/src/assets/logo.png" />
     <HelloWorld msg="Hello Vue 3.0 + Vite" />
     <Counter />
   </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
-import Counter from '../components/Counter.vue';
+import HelloWorld from '/@/components/HelloWorld.vue';
+import Counter from '/@/components/Counter.vue';
 
 export default {
   name: 'Home',
