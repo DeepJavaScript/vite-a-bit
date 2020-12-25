42c7552106d9122c8f8d5a86abf4cacb9766a122
titangene <titangene.tw@gmail.com>
2020-12-25 08:56:25 +0800

    fix(vite-HW): HelloWorld add emit & use composition API

--- vite-hello-world/src/components/HelloWorld.vue
+++ vite-hello-world/src/components/HelloWorld.vue
@@ -1,19 +1,39 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>count: {{ count }}</p>
+  <p>doubleCount: {{ doubleCount }}</p>
+  <button @click="$emit('increment')">increment</button>
+  <button @click="$emit('reset')">reset</button>
 </template>
 
 <script>
+import { computed } from 'vue';
+
 export default {
   name: 'HelloWorld',
+  emits: ['increment', 'reset'],
   props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
+    msg: {
+      type: String,
+      default: ''
+    },
+    count: {
+      type: Number,
+      required: true
     }
+  },
+  setup(props) {
+    const doubleCount = computed(() => props.count * 2);
+
+    return {
+      doubleCount
+    };
   }
 }
 </script>
+
+<style lang="scss" scoped>
+p {
+  margin: 0;
+}
+</style>
--- vite-hello-world/src/views/Home.vue
+++ vite-hello-world/src/views/Home.vue
@@ -3,12 +3,20 @@
     <h1>This is an home page</h1>
     <img alt="Vue logo" src="/@/assets/logo.png" />
     <img alt="Vue logo" src="/src/assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <HelloWorld
+      msg="Hello Vue 3.0 + Vite"
+      :count="count"
+      @increment="increment"
+      @reset="reset"
+    />
+    <hr />
     <Counter />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 import HelloWorld from '/@/components/HelloWorld.vue';
 import Counter from '/@/components/Counter.vue';
 
@@ -17,6 +25,23 @@ export default {
   components: {
     HelloWorld,
     Counter
+  },
+  setup() {
+    const count = ref(0);
+
+    function increment() {
+      count.value++;
+    }
+
+    function reset() {
+      count.value = 0;
+    }
+
+    return {
+      count,
+      increment,
+      reset
+    };
   }
 };
 </script>
