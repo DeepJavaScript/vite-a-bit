4a909a3d1711ffd527eaa94c146c83aad2c0a845
acid7820@gmail.com <acid7820@gmail.com>
2020-12-26 18:34:03 +0800

    add scss in App.vue and delete index.css file

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -11,3 +11,26 @@ export default {
   name: 'App'
 }
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #7c848d;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
