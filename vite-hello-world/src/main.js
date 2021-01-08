a4bc19eaa90ef2a7d0cc7a9a5415d34a4419943a
titangene <titangene.tw@gmail.com>
2021-01-08 08:33:50 +0800

    fix(vite-HW): remove Vue devtools plugin

--- vite-hello-world/src/main.js
+++ vite-hello-world/src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import devtools from './plugins/devtools';
 import router from './router';
 import store from './store';
 import './assets/scss/main.scss';
 
 createApp(App)
-  .use(devtools)
   .use(store)
   .use(router)
   .mount('#app');
