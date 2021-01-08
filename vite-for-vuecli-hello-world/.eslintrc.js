fbbb71e159cb41eb35800d9db5784390220c257b
G100my <G100my.lan229@gmail.com>
2021-01-08 21:34:56 +0800

    init eslint config

---
+++ vite-for-vuecli-hello-world/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:vue/base', 'plugin:vue/vue3-essential', 'plugin:vue/vue3-essential',],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {}
+};
