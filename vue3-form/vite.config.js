d387c11d0e52bc1cdcaf37ff73272547ac5b12b3
G100my <G100my.lan229@gmail.com>
2021-01-12 01:46:23 +0800

    set build.outdir

--- vue3-form/vite.config.js
+++ vue3-form/vite.config.js
@@ -1,8 +1,12 @@
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
+const { resolve } = require('path')
 /**
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    outDir: '/Lo',
+  }
 }
