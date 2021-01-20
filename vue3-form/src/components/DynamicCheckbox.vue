def27e3384c037577e58b24d455554d61625cd6a
acid7820@gmail.com <acid7820@gmail.com>
2021-01-20 20:47:52 +0800

    feat: add base scss, handler dynamic options Array and prop to CheckboxGroup in components/DynamicCheckbox.vue

---
+++ vue3-form/src/components/DynamicCheckbox.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="dynamic-checkbox">
+		<input
+			class="create-checkbox-item"
+			type="text"
+			ref="createCheckboxItem"
+			@change.enter="handler"
+			placeholder="輸入興趣，Enter 後即可新增"
+		>
+		<CheckboxGroup
+			:groupName="groupName"
+			:options="dynamicOptions"
+			@updateSelected="$emit('updateSelected', $event)"
+		></CheckboxGroup>
+	</div>
+</template>
+
+<script>
+import CheckboxGroup from '../components/CheckboxGroup.vue'
+export default {
+	components: {
+    CheckboxGroup
+	},
+	props: {
+		groupName: {
+			type: String,
+			requery: true
+		},
+		options: {
+			type: Array,
+			default: () => []
+		},
+	},
+	mounted () {
+		this.dynamicOptions = [...this.options]
+	},
+	data() {
+		return {
+			dynamicOptions: []
+		}
+	},
+	methods: {
+		handler(event) {
+			if(!event.target.value) return
+
+			const newCheckboxItem = {
+				text: event.target.value,
+				value: event.target.value
+			}
+			this.dynamicOptions = [
+				...this.dynamicOptions,
+				newCheckboxItem
+			]
+			this.$refs.createCheckboxItem.value = null
+		},
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.dynamic-checkbox {
+	.create-checkbox-item {
+		width: 100%;
+		margin-bottom: 5px;
+	}
+}
+
+</style>
