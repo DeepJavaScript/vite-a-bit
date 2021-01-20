82c6f1085c07e58480fb512913463291d858aede
acid7820@gmail.com <acid7820@gmail.com>
2021-01-21 00:59:25 +0800

    feat: add base scss, handler prop and emit value、show or close PW in components/PlainPassword.vue

---
+++ vue3-form/src/components/PlainPassword.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<input
+			:type="inputType"
+			:value="value"
+			@input="$emit('update:psaaword', $event.target.value)"
+		>
+		<button
+			v-if="inputType === 'password'"
+			type="button"
+			@click="inputType = 'text'"
+		>show</button>
+		<button
+			v-else
+			type="button"
+			@click="inputType = 'password'"
+		>close</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		value: {
+			type: String,
+			requey: true
+		},
+	},
+	data() {
+		return {
+			inputType: 'password'
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
