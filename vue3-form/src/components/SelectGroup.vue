60c5be958acd8a1bd3a15b1a9365e1c17c073bf1
acid7820@gmail.com <acid7820@gmail.com>
2021-01-20 23:19:57 +0800

    feat: add base scss, handler prop and emit selected value in components/SelectGroup.vue

---
+++ vue3-form/src/components/SelectGroup.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="select-group">
+		<select
+			:name="groupName"
+			@change="handler"
+		>
+			<option disabled value="volvo">請選擇</option>
+			<template
+				v-for="item in options"
+				:key="item.value"
+			>
+				<option
+					:value="item.value"
+				>{{ item.text }}</option>
+			</template>
+		</select>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		groupName: {
+			type: String,
+			requery: true
+		},
+		options: {
+			type: Array,
+			default: () => []
+		},
+	},
+	methods: {
+		handler(event) {
+			const selectedValue = event.target.value
+			const selectedItem = this.options.find(item => {
+				return item.value === selectedValue
+			}) 
+			this.$emit('updateSelected', selectedItem)
+		}
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.select-group {
+	select {
+		padding: 2px;
+    width: 100%;
+	}
+}
+</style>
