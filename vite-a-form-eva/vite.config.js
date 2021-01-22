8076dc9ef48c3a613165af2f530d881f36facc75
cshif <cooshif@gmail.com>
2021-01-22 17:06:01 +0800

    modified root

--- vite-a-form-eva/vite.config.js
+++ vite-a-form-eva/vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    base: '/vite-a-bit/vue3-form/eva/'
+  }
 }
