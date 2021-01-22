b8ba4b97e5c842fbd25b1082ce17787a45904318
acid7820@gmail.com <acid7820@gmail.com>
2021-01-22 13:09:52 +0800

    feat: add props and emit slider value in components/Slider.vue

---
+++ vue3-form/src/components/Slider.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="slider">
+		<span>{{ min }}</span>
+		<input
+			type="range"
+			class="slider-range"
+			:min="min"
+			:max="max"
+			:value="value"
+			@input="$emit('update:range', $event.target.value)"
+		>
+		<span>{{ max }}</span>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		min: {
+			type: Number,
+			reque: true
+		},
+		max: {
+			type: Number,
+			reque: true
+		},
+		value: {
+			type: Number,
+			reque: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.slider {
+	display: flex;
+	align-items: center;
+	.slider-range {
+		flex: 1 1 auto;
+	}
+}
+</style>
