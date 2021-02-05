baf382b5544ae7e133e25195fd05ae97f89715b0
titangene <titangene.tw@gmail.com>
2021-02-05 15:22:16 +0800

    feat(vue3-form): FormGroup set label-for

--- vue3-form/src/components/FormGroup.vue
+++ vue3-form/src/components/FormGroup.vue
@@ -1,8 +1,13 @@
 <template>
-  <fieldset>
+  <fieldset v-if="!labelFor">
     <legend>{{ label }}</legend>
     <slot />
   </fieldset>
+
+  <div v-else class="form-group">
+    <label :for="labelFor">{{ label }}</label>
+    <slot :label-for="labelFor" />
+  </div>
 </template>
 
 <script>
@@ -12,9 +17,20 @@ export default {
     label: {
       type: String,
       required: true
+    },
+    labelFor: {
+      type: String
     }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.form-group {
+  margin-bottom: 4px;
+
+  label {
+    display: block;
+  }
+}
+</style>
