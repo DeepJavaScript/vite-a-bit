37b97f7b231616503303eda05c5d1bb3ef2e8e3d
titangene <titangene.tw@gmail.com>
2021-02-05 21:07:06 +0800

    feat(vue3-form): normalized form options
    generated form will return directly when the option is an object, and create text and value properties for other types

--- vue3-form/src/composables/useForm.js
+++ vue3-form/src/composables/useForm.js
@@ -3,7 +3,11 @@ import { computed, unref } from 'vue';
 export function generateFormOptions(options) {
   return computed(() =>
     unref(options).map(item => {
-      return { text: item, value: item };
+      if (typeof item === 'object') {
+        return item;
+      } else {
+        return { text: item, value: item };
+      }
     })
   );
 }
