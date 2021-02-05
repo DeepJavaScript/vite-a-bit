8ea3ef21b6409dd87bba033b3317b8ac13847142
titangene <titangene.tw@gmail.com>
2021-02-05 14:59:55 +0800

    feat(vue3-form): FormCheckbox

---
+++ vue3-form/src/components/FormCheckbox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="form-check">
+    <input
+      type="checkbox"
+      :id="$attrs.id"
+      :value="$attrs.value"
+      v-model="value"
+    />
+    <label :for="$attrs.id">
+      <slot />
+    </label>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'FormCheckbox',
+  props: {
+    modelValue: {}
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const value = computed({
+      get: () => props.modelValue,
+      set(value) {
+        emit('update:modelValue', value);
+      }
+    });
+
+    return {
+      value
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
