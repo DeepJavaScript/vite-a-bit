8ce147fc63f5c0f01c3aa6075284ae53ad2571bc
titangene <titangene.tw@gmail.com>
2021-02-05 15:00:26 +0800

    feat(vue3-form): FormGroup

---
+++ vue3-form/src/components/FormGroup.vue
@@ -0,0 +1,20 @@
+<template>
+  <fieldset>
+    <legend>{{ label }}</legend>
+    <slot />
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'FormGroup',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
