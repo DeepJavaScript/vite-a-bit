c809ddb3701385790a920b82715577f03e8b574d
titangene <titangene.tw@gmail.com>
2021-02-05 12:00:00 +0800

    feat(vue3-form): reset file

--- vue3-form/src/components/FormFile.vue
+++ vue3-form/src/components/FormFile.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form-file form-group">
     <label for="image">上傳圖片 (限圖片)</label>
-    <input type="file" id="image" @change="uploadFile" />
+    <input type="file" id="image" ref="input" @change="uploadFile" />
 
     <div class="preview-image">
       <span>{{ previewImage.name }}</span>
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import { reactive, onMounted } from 'vue';
+import { ref, reactive, watch } from 'vue';
 
 export default {
   name: 'FormFile',
@@ -19,30 +19,43 @@ export default {
   props: {
     file: {
       type: Object,
-      required: true
+      defualt: null
     }
   },
   setup(props, { emit }) {
+    const input = ref(null);
     const previewImage = reactive({
       name: '',
       url: ''
     });
 
-    onMounted(() => {
-      if (props.modelValue) {
-        previewImage.url = URL.createObjectURL(props.modelValue);
+    watch(
+      () => props.file,
+      () => {
+        if (props.file === null) resetFile();
       }
-    });
+    );
 
     function uploadFile(event) {
-      console.log(event);
       const fileObject = event.target.files[0];
       previewImage.name = fileObject.name;
       previewImage.url = URL.createObjectURL(fileObject);
       emit('update:file', fileObject);
     }
 
+    function resetFile() {
+      input.value.value = '';
+      console.log('reset input file');
+      resetPreviewImage();
+    }
+
+    function resetPreviewImage() {
+      previewImage.name = '';
+      previewImage.url = '';
+    }
+
     return {
+      input,
       previewImage,
 
       uploadFile
