e5fe54013acd743d09c19062fe6f8e0181f859f2
titangene <titangene.tw@gmail.com>
2021-02-05 15:00:15 +0800

    feat(vue3-form): FormRadioGroup

---
+++ vue3-form/src/components/FormRadioGroup.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="form-check"
+    v-for="(option, index) in options"
+    :key="option.value"
+  >
+    <input
+      type="radio"
+      :id="`${$attrs.id}-options-${index}`"
+      :value="option.value"
+      v-model="value"
+    />
+    <label :for="`${$attrs.id}-options-${index}`">
+      {{ option.text }}
+    </label>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'FormRadioGroup',
+  props: {
+    modelValue: {},
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const value = computed({
+      get: () => props.modelValue,
+      set(value) {
+        emit('update:modelValue', value);
+      }
+    });
+
+    return {
+      value
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
