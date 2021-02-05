30e67387a2ff3a67e3ca5cbfcee5df2ab3b980a7
titangene <titangene.tw@gmail.com>
2021-02-05 16:43:49 +0800

    feat(vue3-form): FormSelect

---
+++ vue3-form/src/components/FormSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <select
+    :id="id"
+    :value="modelValue"
+    @change="$emit('update:modelValue', $event.target.value)"
+  >
+    <option :value="null" disabled>請選擇</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.text }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'FormSelect',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    modelValue: {}
+  },
+  emits: ['update:modelValue']
+};
+</script>
+
+<style lang="scss" scoped></style>
