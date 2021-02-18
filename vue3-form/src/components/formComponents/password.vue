0f388db98675863d82514432d11893153c5b552d
G100my <G100my.lan229@gmail.com>
2021-02-19 00:45:04 +0800

    composition: password-2

--- vue3-form/src/components/formComponents/password.vue
+++ vue3-form/src/components/formComponents/password.vue
@@ -1,27 +1,24 @@
 <template>
-	<input v-model="value" :type="canYouSee ? 'text' : 'password'" />
+	<input
+		@input="$emit('update:modelValue', $event.target.value)"
+		:value="modelValue"
+		:type="canYouSee ? 'text' : 'password'"
+	/>
 	<button @click.prevent="canYouSee = !canYouSee">
 		canYouSee: {{ canYouSee }}
 	</button>
 </template>
 <script>
-import { ref, computed } from "vue";
+import { ref } from "vue";
 export default {
 	props: {
 		modelValue: String,
 	},
 	emits: ["update:modelValue"],
-	setup(props, { emit }) {
+	setup() {
 		const canYouSee = ref(false);
-		console.log(props.modelValue);
-
-		const value = computed({
-			get: () => props.modelValue,
-			set: (newValue) => emit("update:modelValue", newValue),
-		});
 		return {
 			canYouSee,
-			value,
 		};
 	},
 };
