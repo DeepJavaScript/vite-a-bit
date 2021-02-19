384be26407380a2eb88c94e9c52d0d6ad7d4cfc2
titangene <titangene.tw@gmail.com>
2021-02-19 11:59:53 +0800

    chore(vue3-form): use vite-svg-loader

--- vue3-form/vite.config.js
+++ vue3-form/vite.config.js
@@ -1,12 +1,13 @@
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import svgLoader from 'vite-svg-loader';
 
 const pathResolve = dir => path.resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   base: './',
   resolve: {
     alias: {
