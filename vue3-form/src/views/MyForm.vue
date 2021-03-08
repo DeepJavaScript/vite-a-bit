c3244e566c257cb12d160c71ad991b84cefe50b0
Louis <louis222220@gmail.com>
2021-03-08 16:56:15 +0800

    Fix: preventDefault, data-binding

--- vue3-form/src/views/MyForm.vue
+++ vue3-form/src/views/MyForm.vue
@@ -1,36 +1,40 @@
 <template>
-  <form id="myform">
+  <pre>
+    {{ userData }}
+  </pre>
+  <form id="myform" action="" @reset.prevent="onReset" @submit.prevent="onSubmit">
     <p>
       <label for="name">名字：</label>
-      <input type="text" id="name" name="name" />
+      <input v-model="userData.name" type="text" id="name" name="name" />
     </p>
     <p>
       <label for="tel">電話</label>
-      <input type="tel" id="tel" name="tel" />
+      <input v-model="userData.tel" type="tel" id="tel" name="tel" />
     </p>
     <p>
       <label for="date">日期：</label>
-      <input type="date" id="date" name="date" />
+      <input v-model="userData.date" type="date" id="date" name="date" />
     </p>
     <p>
       <label for="time">時間：</label>
-      <input type="time" id="time" name="time" />
+      <input v-model="userData.time" type="time" id="time" name="time" />
     </p>
     <p>
       性別：
-      <input type="radio" id="male" name="gender" value="male" checked />
+      <input v-model="userData.gender" type="radio" id="male" name="gender" value="male" checked />
       <label for="male">生理男</label>
 
-      <input type="radio" id="female" name="gender" value="female" />
+      <input v-model="userData.gender" type="radio" id="female" name="gender" value="female" />
       <label for="female">生理女</label>
 
-      <input type="radio" id="custom" name="gender" value="custom" />
+      <input v-model="userData.gender" type="radio" id="custom" name="gender" value="custom" />
       <label for="custom">多元</label>
     </p>
 
     <label for="interests">興趣：</label>
     <div v-for="(interestOption, index) in interestOptions" :key="index">
       <input
+        v-model="userData.interests"
         type="checkbox"
         :id="interestOption"
         :value="interestOption"
@@ -43,7 +47,7 @@
     <button type="button" @click="addInterestOption">新增興趣</button>
 
     <p>
-      <span v-for="(tag, index) of tags" :key="index">
+      <span v-for="(tag, index) of userData.tags" :key="index">
         &nbsp;
         <span>{{ tag }}</span>
         <span @click="removeTag(index)">x</span>
@@ -55,8 +59,8 @@
     </p>
     <p>
       <label for="location">棲息地：</label>
-      <select id="location" name="residence">
-        <option value="">請選擇</option>
+      <select v-model="userData.location" id="location" name="residence">
+        <option value="" disabled>請選擇</option>
         <option value="tainan">台南</option>
         <option value="taipei">台北</option>
         <option value="other">其他</option>
@@ -64,15 +68,16 @@
     </p>
     <p>
       <label for="age">年齡：</label>
-      <input type="number" id="age" name="age" />
+      <input v-model="userData.age" type="number" id="age" name="age" />
     </p>
     <p>
       <label for="email">Email：</label>
-      <input type="email" id="email" name="email" />
+      <input v-model="userData.email" type="email" id="email" name="email" />
     </p>
     <p>
       <label for="password">Password：</label>
       <input
+        v-model="userData.password"
         :type="isPasswordShown ? 'text' : 'password'"
         id="password"
         name="password"
@@ -86,11 +91,11 @@
 
     <p>
       <label for="feeling">體驗感受</label>
-      <input type="range" id="feeling" name="feeling" min="1" max="3" />
+      <input v-model="userData.feeling" type="range" id="feeling" name="feeling" min="1" max="3" />
     </p>
     <p>
       <label for="comment">備註</label>
-      <textarea form="myform" rows="3" id="comment" name="comment"></textarea>
+      <textarea v-model="userData.comment" form="myform" rows="3" id="comment" name="comment"></textarea>
     </p>
     <p>
       <label for="photo">照片</label>
@@ -105,7 +110,7 @@
     </p>
     <p>
       <label>預覽</label>
-      <img v-for="(photo, index) of photos" :key="index" :src="photo.url" />
+      <img v-for="(photo, index) of userData.photos" :key="index" :src="photo.url" />
     </p>
     <p>
       <input type="reset" value="Reset" />
@@ -118,14 +123,15 @@
 export default {
   data() {
     return {
+      userData: {},
       interestOptions: ["寫程式", "看電影"],
       newInterest: "",
-      tags: [],
       newTag: "",
       isPasswordShown: false,
-      photo: null,
-      photos: [],
     };
+  },
+  created() {
+    this.onReset();
   },
   methods: {
     addInterestOption() {
@@ -133,19 +139,42 @@ export default {
       this.newInterest = "";
     },
     addTag() {
-      this.tags.push(this.newTag);
+      this.userData.tags.push(this.newTag);
       this.newTag = "";
     },
     removeTag(index) {
-      this.tags.splice(index, 1);
+      this.userData.tags.splice(index, 1);
     },
     onPhotoChange(event) {
       const files = [...event.target.files];
-      this.photos = files.map((file) => {
+      this.userData.photos = files.map((file) => {
         return {
           url: URL.createObjectURL(file),
         };
       });
+    },
+    onReset() {
+      this.userData = this.initialData;
+    },
+  },
+  computed: {
+    initialData() {
+      return {
+        name: 'louis',
+        tel: null,
+        date: null,
+        time: null,
+        gender: null,
+        interests: [],
+        tags: [],
+        location: null,
+        age: null,
+        email: null,
+        password: null,
+        feeling: null,
+        comment: null,
+        photos: [],
+      };
     },
   },
 };
